7a553037dcced594065e4aa42430489dd3d91da7
abhishek <abbhishekk27@gmail.com>
2024-03-14 07:22:02 +0530

    refactor: contract

--- src/utils/contract/contract.ts
+++ src/utils/contract/contract.ts
@@ -1,8 +1,12 @@
-/* eslint-disable unused-imports/no-unused-vars */
-/* eslint-disable @typescript-eslint/naming-convention */
-import type { Program } from '@coral-xyz/anchor';
-import * as anchor from '@coral-xyz/anchor';
-import type NodeWallet from '@coral-xyz/anchor/dist/cjs/nodewallet';
+import {
+  AnchorProvider,
+  BN,
+  type Idl,
+  Program,
+  utils,
+  type Wallet,
+  web3,
+} from '@coral-xyz/anchor';
 import * as spl from '@solana/spl-token';
 import { LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 
@@ -11,115 +15,116 @@ import { type EarnReloaded, IDL } from './program';
 const PROGRAM_ID = process.env.NEXT_PUBLIC_PAYMENT_PROGRAM_ID || '';
 const RPC_URL = process.env.NEXT_PUBLIC_PAYMENT_RPC_URL || '';
 
-export const connection = new anchor.web3.Connection(RPC_URL, 'confirmed');
+export const connection = new web3.Connection(RPC_URL, 'confirmed');
 
-const getProvider = (wallet: anchor.Wallet) => {
-  const opts = {
-    preflightCommitment: 'processed' as anchor.web3.ConfirmOptions,
-  };
-
-  const provider = new anchor.AnchorProvider(
-    connection,
-    wallet,
-    opts.preflightCommitment,
-  );
-  return provider;
+const getProvider = (wallet: Wallet) => {
+  return new AnchorProvider(connection, wallet, {
+    preflightCommitment: 'processed',
+  });
 };
 
-const anchorProgram = (wallet: anchor.Wallet) => {
+const getAnchorProgram = (wallet: Wallet) => {
   const provider = getProvider(wallet);
-  const idl = IDL as anchor.Idl;
-  const program = new anchor.Program(
+  const idl = IDL as Idl;
+  return new Program(
     idl,
     PROGRAM_ID,
     provider,
   ) as unknown as Program<EarnReloaded>;
-
-  return program;
 };
+
+const getOrCreateTokenAccount = async (
+  token: PublicKey,
+  owner: PublicKey,
+  payer: PublicKey,
+) => {
+  const ata = await spl.getAssociatedTokenAddress(
+    token,
+    owner,
+    false,
+    spl.TOKEN_PROGRAM_ID,
+    spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+  );
+  const info = await connection.getAccountInfo(ata);
+  let instruction = null;
+  if (!info) {
+    instruction = spl.createAssociatedTokenAccountInstruction(
+      payer,
+      ata,
+      owner,
+      token,
+    );
+  }
+  return { ata, instruction };
+};
+
 export const createPaymentSPL = async (
-  anchorWallet: NodeWallet,
-  receiver: anchor.web3.PublicKey,
+  wallet: Wallet,
+  receiver: PublicKey,
   amount: number,
-  token: anchor.web3.PublicKey,
+  token: PublicKey,
   id: string,
 ) => {
-  const program = anchorProgram(anchorWallet);
-  const [payout_account] = anchor.web3.PublicKey.findProgramAddressSync(
+  const program = getAnchorProgram(wallet);
+  const [payoutAccount] = PublicKey.findProgramAddressSync(
     [
-      anchor.utils.bytes.utf8.encode('payout'),
-      anchor.utils.bytes.utf8.encode(id),
+      utils.bytes.utf8.encode('payout'),
+      utils.bytes.utf8.encode(id),
       token.toBuffer(),
       receiver.toBuffer(),
     ],
     program.programId,
   );
-  const receiver_usdc_ata = await spl.getAssociatedTokenAddress(
+
+  const { ata: receiverATA, instruction: receiverATAIx } =
+    await getOrCreateTokenAccount(token, receiver, wallet.publicKey);
+  const { ata: senderATA } = await getOrCreateTokenAccount(
     token,
-    receiver,
-    false,
-    spl.TOKEN_PROGRAM_ID,
-    spl.ASSOCIATED_TOKEN_PROGRAM_ID,
+    wallet.publicKey,
+    wallet.publicKey,
   );
-  const usdc_ata = await spl.getAssociatedTokenAddress(
-    token,
-    anchorWallet.publicKey,
-    false,
-    spl.TOKEN_PROGRAM_ID,
-    spl.ASSOCIATED_TOKEN_PROGRAM_ID,
-  );
-  const info = await connection.getAccountInfo(receiver_usdc_ata);
-  let tokenAccountIx;
-  if (!info) {
-    tokenAccountIx = spl.createAssociatedTokenAccountInstruction(
-      anchorWallet.publicKey,
-      receiver_usdc_ata,
-      receiver,
-      token,
-    );
-  }
-  const ix = await program.methods
-    .payout(id, receiver, new anchor.BN(amount))
+
+  const payoutIx = await program.methods
+    .payout(id, receiver, new BN(amount))
     .accounts({
-      authority: anchorWallet.publicKey,
-      tokenAtaReceiver: receiver_usdc_ata,
-      tokenAtaSender: usdc_ata,
+      authority: wallet.publicKey,
+      tokenAtaReceiver: receiverATA,
+      tokenAtaSender: senderATA,
       tokenMint: token,
-      payoutAccount: payout_account,
+      payoutAccount,
       associatedTokenProgram: spl.ASSOCIATED_TOKEN_PROGRAM_ID,
       tokenProgram: spl.TOKEN_PROGRAM_ID,
     })
     .instruction();
 
-  return [ix, tokenAccountIx];
+  return [payoutIx, receiverATAIx].filter((ix) => ix !== null);
 };
 
 export const createPaymentSOL = async (
-  anchorWallet: NodeWallet,
-  receiver: anchor.web3.PublicKey,
+  wallet: Wallet,
+  receiver: PublicKey,
   amount: number,
   id: string,
 ) => {
-  const program = anchorProgram(anchorWallet);
+  const program = getAnchorProgram(wallet);
 
-  const [payout_account] = await PublicKey.findProgramAddress(
+  const [payoutAccount] = PublicKey.findProgramAddressSync(
     [
-      anchor.utils.bytes.utf8.encode('payout'),
-      anchor.utils.bytes.utf8.encode(id),
-      anchor.utils.bytes.utf8.encode('sol'),
+      utils.bytes.utf8.encode('payout'),
+      utils.bytes.utf8.encode(id),
+      utils.bytes.utf8.encode('sol'),
       receiver.toBuffer(),
     ],
     program.programId,
   );
-  const ix = await program.methods
-    .payoutSol(id, receiver, new anchor.BN(amount * LAMPORTS_PER_SOL))
+
+  return program.methods
+    .payoutSol(id, receiver, new BN(amount * LAMPORTS_PER_SOL))
     .accounts({
-      authority: anchorWallet.publicKey,
-      payoutAccount: payout_account,
+      authority: wallet.publicKey,
+      payoutAccount,
       receiverAccount: receiver,
       tokenProgram: spl.TOKEN_PROGRAM_ID,
     })
     .instruction();
-
-  return ix;
 };
